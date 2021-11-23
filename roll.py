dd0a3de1a3aafcd3dd28b7b583aa8ca8b41b4195
Drake Dwornik <drake@drakedwornik.com>
2021-11-22 22:38:22 -0500

    a command line die roller, can roll multiple dice and set the number of sides

--- roll.py
+++ roll.py
@@ -1,7 +1,72 @@
-# roll a "die" some number of times.
-# roll - run it once and go into a loop
-# roll 1 - produce a number 1-6 random and print it
-# roll 2 - produce two numbers 1-6 and print them
-# roll 3 - produce three numbers and print them.
-#
+#!python3
+import getopt
+from random import randrange
+import sys
 
+
+def roll_die(sides: int = 6) -> int:
+    return randrange(0, sides) + 1
+
+
+def roll_multiple(number: int, sides: int = 6):
+    for roll_number in range(0, number):
+        roll = roll_die(sides)
+        print(f"roll number {roll_number}: {roll}")
+
+def roll_interactive(sides: int = 6):
+    roll_number = 1
+    should_roll = True
+    while should_roll:
+        roll = roll_die(sides)
+        print(f"roll number {roll_number} of a {sides} sided die: {roll}")
+        should_roll = roll_again()
+        roll_number += 1
+
+
+def roll_again():
+    should_check = True
+    while should_check:
+        cont = input("roll again? (y/n): ")
+        try:
+            cont = cont.upper()
+        except (ValueError, TypeError):
+            should_check = True
+        else:
+            if cont == 'Y':
+                return True
+            elif cont == 'N':
+                return False
+
+def get_args(args):
+    opts, args = getopt.getopt(args, "hn:s:",["help"])
+    # print((opts, args))
+    return (opts, args)
+
+def cli(args):
+    opts, args = get_args(args)
+    sides = 6
+    interactive = True
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print(help_text)
+            exit()
+        elif opt == '-s':
+            sides = int(arg)
+        elif opt == "-n":
+            interactive = False
+            number = int(arg)
+    if interactive:
+        roll_interactive(sides)
+    if not interactive:
+        roll_multiple(number,sides)
+
+help_text = """
+This program is a die roller
+-s option sets the number of sides (not bound by the physical realm)
+-n option sets the number of rolls (default 6)
+-h or --help prints this text
+
+if run without -n interactive mode is entered
+"""
+if __name__ == '__main__':
+    cli(sys.argv[1:])
